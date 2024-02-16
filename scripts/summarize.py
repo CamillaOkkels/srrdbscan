dfc68d54caa2929ec164bfc4c53416727f01ef0e
Martin Aumueller <maau@itu.dk>
2024-02-16 09:37:06 +0100

    updated to new file labeling.

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -5,8 +5,10 @@ import sys
 fn = sys.argv[1]
 gn = sys.argv[2]
 
-eps = int(fn.split("_")[2])
-minPts = int(fn.split("_")[4])
+print(fn.split("_"))
+
+eps = int(fn.split("_")[4])
+minPts = int(fn.split("_")[6])
 
 labels_pred = h5py.File(fn)['grp']['labels']
 labels_true = h5py.File(gn)[f'eps={eps}, minPts={minPts}']['clustering_labels']

de29278104be3312033bd5dc3ed0f558cc2558fd
Martin Aumueller <maau@itu.dk>
2024-03-21 11:23:16 +0100

    debugging info

--- python/wrapper/python_wrapper.cpp
+++ python/wrapper/python_wrapper.cpp
@@ -92,6 +92,7 @@ public:
         dbscan->performClustering();
 
         for(auto p: ds->points){
+            std::cout << p.print() << std::endl;
             labels.push_back(p.print());
         }
 

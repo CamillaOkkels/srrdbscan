bcd48c9191020041f7f090b4ae5a3669ef550bb1
Martin Aumueller <maau@itu.dk>
2025-03-19 08:45:34 +0100

    make python test script more excplicit

--- python/test/test.py
+++ python/test/test.py
@@ -1,15 +1,38 @@
 import dbscan_srr as dbscan
 import numpy as np
-import sys
-import h5py
+from sklearn.datasets import make_blobs
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
 
-f = h5py.File(sys.argv[1], "r")
-eps = float(sys.argv[2])
-minPts = int(sys.argv[3])
-X = np.array(f['data'])
+# create 5 clusters
+X,y = make_blobs(n_samples=10_000, n_features=100, centers=5, random_state=0)
+eps = 15
+minPts = 100
 
+# run our dbscan implementation to retrieve cluster labels
 srr = dbscan.SRR()
 
-srr.fit_predict(X, 0.5, 3, True, "test", 56, -1, 1, eps, minPts, 1.0)
+labels = srr.fit_predict(X, 
+    0.9, # probability of missing a point
+    1, # memory constraint in GB
+    False, # benchmark?
+    "test", # name of file for some default output
+    1, # number of threads
+    -1, # use best level dynamically
+    1,  # shrinkage factor to avoid looking at all repetitions in a level
+    eps, 
+    minPts, 
+    1.0) # the approximation factor
+
+# print some statistics
 print(srr.statistics())
+
+# compute exact groundtruth labels
+gt_labels = DBSCAN(eps=eps, min_samples=minPts).fit(X).labels_
+
+# report on quality score compared to exact DBSCAN, and the actual groundtruth labels
+print("ARI compared to DBSCAN", adjusted_rand_score(gt_labels, labels), " NMI: ", normalized_mutual_info_score(gt_labels, labels))
+print("ARI compared to groundtruth labels:", adjusted_rand_score(y, labels), " NMI: ", normalized_mutual_info_score(y, labels))
+
+
 
